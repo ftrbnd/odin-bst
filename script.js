0c7c94ecf9577325e7a81abc293f65706337a297
ftrbnd <giosalas25@gmail.com>
2023-04-10 14:53:06 -0700

    Implement insert, delete, find

--- script.js
+++ script.js
@@ -24,6 +24,62 @@ class Tree {
 
         return node;
     }
+
+    insert(value) {
+        if (this.root == null) {
+            this.root = new Node(value);
+            return this.root;
+        }
+
+        if (value < this.root.val) {
+            this.root.left = this.insert(this.root.left, value);
+        } else {
+            this.root.right = this.insert(this.root.right, value);
+        }
+
+        return this.root;
+    }
+
+    delete(value) {
+        if (this.root == null) return this.root;
+
+        if (value < this.root.val) {
+            this.root.left = this.delete(this.root.left, value);
+        } else if (value > this.root.val) {
+            this.root.right = this.delete(this.root.right, value);
+        } else {
+            if (this.root.left == null) return this.root.right;
+            if (this.root.right == null) return this.root.left;
+
+            this.root.val = this.minValue(this.root.right);
+  
+            this.root.right = this.delete(this.root.right, this.root.key);
+        }
+  
+        return this.root;
+    }
+
+    minValue(root) {
+        let min = root.val;
+        while (root.left != null) {
+            min = root.left.val;
+            root = root.left;
+        }
+        return min;
+    }
+
+    find(value, node = this.root) {
+        if (node == null) return null;
+        if (value == node.val) return node;
+
+        if (value < node.val) {
+            node = this.find(value, node.left)
+        } else {
+            node = this.find(value, node.right);
+        }
+
+        return node;
+    }
 }
 
 // https://www.theodinproject.com/lessons/javascript-binary-search-trees
@@ -42,3 +98,6 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 prettyPrint(tree.root)
+
+console.log('finding 2: ', tree.find(2));
+console.log('finding 67: ', tree.find(67));
